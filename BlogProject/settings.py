89d0fb408c5b5d9c2f5b43f6e39b37740d165e90
TranHuy <58626944+TranHuy2707@users.noreply.github.com>
2020-07-25 00:21:31 +0700

    Update settings.py

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(a-k*+n=n76ygsjlq7jpx!l2w8(v*t#bh=(ss-i@n_xmbxk#n9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://tranhuyit.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
